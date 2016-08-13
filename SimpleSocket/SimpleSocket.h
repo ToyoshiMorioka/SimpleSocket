39b52f9537c9bc5c61e2895433d9a49d3fc84ef9
morioka <morioka@1-10.com>
2016-08-13 16:25:30 +0900

    Carthageに対応。

---
+++ SimpleSocket/SimpleSocket.h
@@ -0,0 +1,19 @@
+//
+//  SimpleSocket.h
+//  SimpleSocket
+//
+//  Created by MORIOKAToyoshi on 2016/08/13.
+//  Copyright © 2016年 ___MORIOKAToyoshi___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleSocket.
+FOUNDATION_EXPORT double SimpleSocketVersionNumber;
+
+//! Project version string for SimpleSocket.
+FOUNDATION_EXPORT const unsigned char SimpleSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleSocket/PublicHeader.h>
+
+
